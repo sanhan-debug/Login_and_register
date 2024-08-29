e4ae53b44c9396b08ba203a016be93df7d4d5c0e
sanhan-debug <snhansayilov@gmail.com>
2024-08-29 13:47:25 +0400

    v4

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = express()
 dotenv.config();
 app.use(express.static("public"))
 app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 const PORT = process.env.SERVER_PORT
 const URI = process.env.URI
 
@@ -24,10 +25,31 @@ app.get('/register',(req,res)=>{
     res.sendFile(path.resolve("./views/register.html"))
 })
 
+app.get('/search',(req,res)=>{
+    res.sendFile(path.resolve('./views/chekUser.html'))
+})
+
 app.post('/register',customizedMulter.single('avatar'),(req,res)=>{
     const {username,surname,age,telephone,email,password} = req.body
+    const {filename} = req.file
+    console.log(req.file)
     const user = userModel.create({username,surname,age,telephone,email,password,photo:`/${filename}`})
     res.send(user)
+    console.log(user)
+})
+
+
+app.post('/search',async (req,res)=>{
+    const usernameInput = req.body.username
+    // console.log(usernameInput)
+    try{
+        const user = await userModel.findOne({username:usernameInput})
+        if(user){
+            res.json(user)
+        }
+    }catch(err){
+        console.error(err)
+    }
 })
 
 
